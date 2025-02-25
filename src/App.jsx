cd48e4d6b5df7a5133ffa68b3246ae1f79ef5621
Dilasha Ghimire <112413218+dilasha-ghimire@users.noreply.github.com>
2025-02-25 10:11:34 +0545

    update: ui refactor (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Login from "./components/authentication/Login.jsx";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-600">Hello World!!!</h1>
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 
